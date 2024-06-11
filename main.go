8a1ca3ea9eada8cca95e133418d481503e749eb4
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2024-06-11 18:36:57 +0100

    altering fe

--- main.go
+++ main.go
@@ -1,44 +1,18 @@
-// main.go
-
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
     r := gin.Default()
 
+    r.Static("/images", "./images")
+
     r.LoadHTMLGlob("html-files/*")
 
     r.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "about-us.html", gin.H{
-            "title": "Welcome to Bark & Beyond!",
-            "content": "Welcome to Bark & Beyond!",
-        })
-    })
-
-    r.GET("/about", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "about-us.html", gin.H{
-            "title": "About Us",
-            "content": "This is the About Us page content.",
-        })
-    })
-
-    r.GET("/services", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "services.html", gin.H{
-            "title": "Services",
-            "content": "This is the Services page content.",
-        })
-    })
-
-    r.GET("/reviews", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "reviews.html", gin.H{
-            "title": "Reviews",
-            "content": "This is the Reviews page content.",
-        })
+        c.HTML(200, "homepage.html", gin.H{})
     })
 
     r.Run(":8080")
