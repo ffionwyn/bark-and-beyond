3203688bf9903480c13df085fb955dda3c455e49
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2024-06-11 15:11:56 +0100

    editing about us

--- bark-and-beyond/src/pages/about-us.js
+++ bark-and-beyond/src/pages/about-us.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function AboutUs() {
   return (
-    <div>
-      <h2>About Us</h2>
-      <p>Welcome to Bark and Beyond!</p>
+    <div className="container">
+      <div className="jumbotron mt-3">
+        <h1 className="display-4">About Us</h1>
+        <p className="lead">Welcome to Bark and Beyond!</p>
+        <hr className="my-4" />
+        <p>Front page stuff</p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
     </div>
   );
 }
