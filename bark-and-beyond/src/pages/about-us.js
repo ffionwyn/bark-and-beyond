da87b5f15d15a4cb7e5e96960d6b7bb572223af3
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2024-06-11 15:04:47 +0100

    created about us and navigation

---
+++ bark-and-beyond/src/pages/about-us.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Welcome to Bark and Beyond!</p>
+    </div>
+  );
+}
+
+export default AboutUs;
