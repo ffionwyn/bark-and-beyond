d80932320319dcb500c5d2a0a79097a5cffc0b1d
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2024-06-11 17:46:22 +0100

    update main

--- main.go
+++ main.go
@@ -5,22 +5,41 @@ package main
 import (
 	"net/http"
 
-	"github.com/ffiongriffiths/bark-and-beyond/pages"
 	"github.com/gin-gonic/gin"
 )
-
 
 func main() {
     r := gin.Default()
 
+    r.LoadHTMLGlob("html-files/*")
+
     r.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Welcome to Bark & Beyond!")
+        c.HTML(http.StatusOK, "about-us.html", gin.H{
+            "title": "Welcome to Bark & Beyond!",
+            "content": "Welcome to Bark & Beyond!",
+        })
     })
 
-	// routes
-    r.GET("/about", pages.AboutUsHandler)
-    r.GET("/services", pages.ServicesHandler)
-    r.GET("/reviews", pages.ReviewsHandler)
+    r.GET("/about", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "about-us.html", gin.H{
+            "title": "About Us",
+            "content": "This is the About Us page content.",
+        })
+    })
+
+    r.GET("/services", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "services.html", gin.H{
+            "title": "Services",
+            "content": "This is the Services page content.",
+        })
+    })
+
+    r.GET("/reviews", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "reviews.html", gin.H{
+            "title": "Reviews",
+            "content": "This is the Reviews page content.",
+        })
+    })
 
     r.Run(":8080")
 }
