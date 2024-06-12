0d673ad8a84194840e214e1a3b9a0654e9a91a59
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2024-06-12 18:43:09 +0100

    save

--- main.go
+++ main.go
@@ -18,5 +18,17 @@ func main() {
         c.HTML(200, "contact.html", gin.H{})
     })
 
+    r.GET("/about", func(c *gin.Context) {
+        c.HTML(200, "about-us.html", gin.H{})
+    })
+
+    r.GET("/services", func(c *gin.Context) {
+        c.HTML(200, "services.html", gin.H{})
+    })
+
+    r.GET("/reviews", func(c *gin.Context) {
+        c.HTML(200, "reviews.html", gin.H{})
+    })
+
     r.Run(":8080")
 }
